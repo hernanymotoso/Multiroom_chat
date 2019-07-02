da8f9a1e77af519c3f1f6a327093803ec39cf3bf
Hernany <contatohernany@gmail.com>
2019-07-02 16:12:05 -0300

    Atualizai o app.js, o controller chat, a view chat,  projeto finalizado

--- app.js
+++ app.js
@@ -11,13 +11,37 @@ var io = require('socket.io').listen(server);
 app.set('io', io);
 
 /* Criar a conexão por websocket */
-io.on('connection', function(socket){
-
-     
-    
+io.on('connection', function(socket){     
     console.log('Usuario conectou');
 
     socket.on('disconnect', function(){
         console.log('Usuario desconectou ')
+    });
+
+    socket.on('msgParaServidor', function(data){
+
+        /* dialogo */
+        socket.emit(
+            'msgParaCliente',
+            {apelido: data.apelido, mensagem: data.mensagem}
+        );
+
+        socket.broadcast.emit(
+            'msgParaCliente',
+            {apelido: data.apelido, mensagem: data.mensagem}
+        );
+
+        /* participantes */
+        if(parseInt(data.apelido_atualizado_nos_clientes) == 0){
+            socket.emit(
+                'participantesParaCliente',
+                {apelido: data.apelido}
+            );
+
+            socket.broadcast.emit(
+                'participantesParaCliente',
+                {apelido: data.apelido}
+            );
+        }
     });
 });
